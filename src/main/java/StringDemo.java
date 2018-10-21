040d537a23ef0eca1dba7d19a0a36009eb14c6c3
vij@1561992 <vijendra1561992@gmail.com>
2018-10-21 14:14:55 +0530

    ways to create java string

--- src/main/java/StringDemo.java
+++ src/main/java/StringDemo.java
@@ -1,5 +1,18 @@
 public class StringDemo {
     public static void main(String[] args){
         System.out.println("first Maven project ");
+
+        //ways to create Java String Object
+        //1: by String literals
+        String s1 = "Vijendra";
+        System.out.println(s1);
+        //2: by new Keyword
+        String s2 = new String("ranita");
+        System.out.println(s2);
+
+        //you can create String object like this alsobut it is same as 1
+        char ch[] = {'j','a','v','a'};
+        String s3 = new String(ch);
+        System.out.println(s3);
     }
 }
